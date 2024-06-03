558aa00ab24338a2734a6b007849f382c90fe850
Yahya Kayani <42173629+Yahyakiani@users.noreply.github.com>
2024-06-03 16:58:51 -0300

    Added  animated record screen

--- src/screens/RecordScreen.jsx
+++ src/screens/RecordScreen.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 import SentenceDisplayComponent from '../components/SentenceDisplayComponent'; // Adjust the path as necessary
 import AudioRecorderComponent from '../components/AudioRecorder';
-import { NavigationContainer } from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
 
 const RecordScreen = ({ navigation }) => {
   const [audioPath, setAudioPath] = useState('');
@@ -13,21 +13,40 @@ const RecordScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <SentenceDisplayComponent
-        currentSentence={currentSentence}
-        setCurrentSentence={setCurrentSentence}
-      />
-      <AudioRecorderComponent
-        onRecordingComplete={handleRecordingComplete}
-        currentSentence={currentSentence}
-        navigation={navigation}
-      />
-    </View>
+    <>
+      <LottieView
+        source={require('../../assets/animations/confetti.json')} // Adjust the path to your confetti animation file
+        autoPlay
+        loop
+        style={styles.lottie}
+        />
+      <View style={styles.container}>
+        <SentenceDisplayComponent
+          currentSentence={currentSentence}
+          setCurrentSentence={setCurrentSentence}
+          />
+        <AudioRecorderComponent
+          onRecordingComplete={handleRecordingComplete}
+          currentSentence={currentSentence}
+          navigation={navigation}
+          />
+      </View>
+          </>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: -3,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    resizeMode: 'cover',
+    
+  },
+  lottie: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: -1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
